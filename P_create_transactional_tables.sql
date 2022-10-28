465979eab17a7e705719f4eac7f12febf8098925
Franco Pasqualino <fpasqualino@frba.utn.edu.ar>
2022-10-28 13:01:12 -0300

    Ordenes de creacion

--- P_create_transactional_tables.sql
+++ P_create_transactional_tables.sql
@@ -16,4 +16,14 @@ BEGIN
         nombre_localidad NVARCHAR(255)
 		);
 
+
+	-- ORDEN: Cliente, Proveedor, Producto, Variante, Producto_variante, (Venta, envio, canal, medio_pago_venta) ALTER, (compra, medio_pago_compra) ALTER, producto_comprado, producto_vendido, descuento_venta, descuento_compra--
+	/** ALTER TABLE [nombre]
+	    ADD CONSTRAINT FK_[tablaOrigen]_[tablaDestino]
+		FOREIGN KEY [nombreColumna] REFERENCES [tablaDestino]([PKDestino])
+	  **/
+
+
 END 
+
+EXEC CREATE_TRANSACTIONAL_TABLES;
