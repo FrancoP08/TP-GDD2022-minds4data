89b36932d4b24da6717be68fc6fa0e43614a1a46
Agustina <arazanov@frba.utn.edu.ar>
2022-11-02 10:15:33 -0300

    fix

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -230,8 +230,8 @@ BEGIN
 		producto_variante_codigo NVARCHAR(50) PRIMARY KEY,
 		producto_codigo NVARCHAR(50) NOT NULL REFERENCES producto,
 		variante_codigo INTEGER NOT NULL REFERENCES variante,
-		precio_actual DECIMAL(18,2) NOT NULL,
-		stock_disponible DECIMAL(18,0) NOT NULL
+		precio_actual DECIMAL(18,2),
+		stock_disponible DECIMAL(18,0)
 	);
 
 	CREATE TABLE producto_comprado (
@@ -322,6 +322,16 @@ BEGIN
 	EXEC SET_IMPORTE 'medio_pago_venta', 'venta_codigo'
 
 	DROP TABLE #importes
+END
+GO
+
+CREATE TRIGGER UPDATE_PORCENTAJE ON venta
+AFTER UPDATE AS
+BEGIN
+	UPDATE descuento_venta
+	SET porcentaje = CONVERT(DECIMAL(10,2), venta_descuento_importe/importe)
+	FROM descuento_venta JOIN inserted i
+	ON descuento_venta.venta_codigo = i.venta_codigo
 END
 GO
 
@@ -477,6 +487,24 @@ BEGIN
 	FROM GD2C2022.gd_esquema.Maestra m
 	JOIN tipo_medio_pago t ON t.tipo_mp = m.VENTA_MEDIO_PAGO
 
+	-- DESCUENTO
+
+	INSERT INTO tipo_descuento_venta (venta_descuento_concepto)
+	SELECT DISTINCT VENTA_DESCUENTO_CONCEPTO
+	FROM GD2C2022.gd_esquema.Maestra
+	WHERE VENTA_DESCUENTO_CONCEPTO IS NOT NULL
+
+	INSERT INTO descuento_venta (venta_codigo, venta_descuento_importe, tipo_descuento_codigo)
+	SELECT DISTINCT m.VENTA_CODIGO, VENTA_DESCUENTO_IMPORTE, tipo_descuento_codigo
+	FROM GD2C2022.gd_esquema.Maestra m
+	JOIN tipo_descuento_venta d ON m.VENTA_DESCUENTO_CONCEPTO = d.venta_descuento_concepto
+	WHERE VENTA_DESCUENTO_IMPORTE IS NOT NULL
+
+	INSERT INTO descuento_compra (descuento_compra_codigo, compra_codigo, descuento_compra_valor)
+	SELECT DISTINCT DESCUENTO_COMPRA_CODIGO, COMPRA_NUMERO, DESCUENTO_COMPRA_VALOR
+	FROM GD2C2022.gd_esquema.Maestra
+	WHERE DESCUENTO_COMPRA_CODIGO IS NOT NULL
+
 	-- PRODUCTO
 
 	INSERT INTO tipo_variante (tipo_variante_descripcion)
@@ -530,25 +558,6 @@ BEGIN
 	FROM GD2C2022.gd_esquema.Maestra
 	WHERE PRODUCTO_VARIANTE_CODIGO IS NOT NULL AND VENTA_CODIGO IS NOT NULL
 	GROUP BY VENTA_CODIGO, PRODUCTO_VARIANTE_CODIGO, VENTA_PRODUCTO_PRECIO
-
-	-- DESCUENTO
-
-	INSERT INTO tipo_descuento_venta (venta_descuento_concepto)
-	SELECT DISTINCT VENTA_DESCUENTO_CONCEPTO
-	FROM GD2C2022.gd_esquema.Maestra
-	WHERE VENTA_DESCUENTO_CONCEPTO IS NOT NULL
-
-	INSERT INTO descuento_venta (venta_codigo, venta_descuento_importe, tipo_descuento_codigo, porcentaje)
-	SELECT DISTINCT m.VENTA_CODIGO, VENTA_DESCUENTO_IMPORTE, tipo_descuento_codigo, CONVERT(DECIMAL(10,2), VENTA_DESCUENTO_IMPORTE/importe)
-	FROM GD2C2022.gd_esquema.Maestra m
-	JOIN tipo_descuento_venta d ON m.VENTA_DESCUENTO_CONCEPTO = d.venta_descuento_concepto
-	JOIN venta v ON v.venta_codigo = m.VENTA_CODIGO
-	WHERE VENTA_DESCUENTO_IMPORTE IS NOT NULL
-
-	INSERT INTO descuento_compra (descuento_compra_codigo, compra_codigo, descuento_compra_valor)
-	SELECT DISTINCT DESCUENTO_COMPRA_CODIGO, COMPRA_NUMERO, DESCUENTO_COMPRA_VALOR
-	FROM GD2C2022.gd_esquema.Maestra
-	WHERE DESCUENTO_COMPRA_CODIGO IS NOT NULL
 END
 GO
 
